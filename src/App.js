40dadfa9ddc49cdfde80af6b4f31d163b77b8ff3
Hinesh Kumar <hineshkumar46@gmail.com>
2022-08-26 01:30:37 +0500

    video 10 complete

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
+// import AboutUs from './components/About';
 import TextForm from './components/TextForm';
+
 
 
 function App() {
@@ -9,7 +11,8 @@ function App() {
     <>
     <Navbar title="Textutals World"></Navbar>
     <div className='container my-3'>
-    <TextForm heading="Enter to Text Analyze below"></TextForm>
+      <TextForm heading="Enter to Text Analyze below"></TextForm>    
+      {/* <AboutUs></AboutUs> */}
     </div>
     
     </>
